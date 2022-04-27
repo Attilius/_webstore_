002cf3d8edb75b60e8233a583a2c888a8eb9a8a2
Attilius <leo20@freemail.hu>
2022-04-27 21:16:53 +0200

    I added ContactFormType.php

---
+++ src/Form/ContactFormType.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\CustomerContact;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactFormType extends AbstractType
+{
+    
+}
