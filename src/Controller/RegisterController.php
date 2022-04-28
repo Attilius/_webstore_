2a07d9f0a778ffa7ac1759553e4ec620d548db31
Attilius <leo20@freemail.hu>
2022-04-28 19:46:20 +0200

    I modified RegisterController.php

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -11,8 +11,8 @@ class RegisterController extends AbstractController
     #[Route('/register', name: 'app_register')]
     public function index(): Response
     {
-        return $this->render('register/index.html.twig', [
-            'controller_name' => 'RegisterController',
+        return $this->render('register/register.html.twig', [
+            'pageLabel' => 'Register',
         ]);
     }
 }
